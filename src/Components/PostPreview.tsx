61cd51f907fc394398208926f3a2658d726abac1
mage1711 <zizo17111998@gmail.com>
2021-07-11 16:59:56 +0200

    fix: import issue

--- src/Components/PostPreview.tsx
+++ src/Components/PostPreview.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
-
+import classNames from 'classnames'
 import { Post } from '../types'
 import Axios from 'axios'
 import ActionButton from './ActionButton'
